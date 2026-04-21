6099f1a2942ca80c4bb6fca0e375102a346481cb
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-04-21 14:09:46 +0500

    Learned the most basic manual RAG

--- RAG/raglearn.py
+++ RAG/raglearn.py
@@ -1,9 +1,25 @@
 #Learning RAG Manually
-
-question = input("Enter your Question: ")
-word = question
-
+context = ""
+highest = 0
+list = ["for","what","is","why","when","how","?"]
 with open("data.txt","r") as f:
-    line = f.readline()
-    if(word == line):
-        context = word
+    data = f.readlines()
+    question = input("Enter your Question: ").lower()
+    question = question.split()
+    for words in list:
+            if words in question:
+                question.remove(words)
+    for line in data:
+        match = 0
+        lower_line = line.lower()
+        for keyword in question:
+            if keyword in lower_line:
+                match += 1
+        if(match > highest):
+            highest = match
+            context = line
+
+print(context)
+
+    
+    
