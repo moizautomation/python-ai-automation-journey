f856608fb35c220bd0d7746379669d022cdbfe41
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-04-23 12:49:36 +0500

    Completed RAG for pdf Q/A - not perfect

--- RAG/raglearn.py
+++ RAG/raglearn.py
@@ -1,12 +1,14 @@
 #Learning RAG Manually
 # from openai import OpenAI
 import google.generativeai as genai
-import os                       # 1. Add this (helps Python talk to your system)
+import os                       #helps Python talk to your system
 from dotenv import load_dotenv
+from pypdf import PdfReader
 
 #This line opens and reads your .env file
 load_dotenv()
 
+#logging into GeminiAI using secret  API key
 genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 model = genai.GenerativeModel('gemini-2.5-flash')
 # my_secret_key = os.getenv("OPENAI_API_KEY")
@@ -14,41 +16,51 @@ model = genai.GenerativeModel('gemini-2.5-flash')
 # #logging into OpenAI using secret key
 # client = OpenAI(api_key=my_secret_key)
 
-
+text = ""
 context = ""
 highest = 0
 
 #list of words to remove from question
 ignore_words = ["for","what","is","why","when","how","?"]
 
-with open("data.txt","r") as f:
-    #reading all the lines
-    data = f.readlines()
+#this will create a reader and open the pdf file
+reader = PdfReader("sample.pdf")
 
-    question = input("Enter your Question: ").lower()
-    tempq = question
-    #splitting the question into a list of words
-    question = question.split()
+#for reading the pages
+for page in reader.pages:
+     text += page.extract_text()
+
+#.splitlines turn it into lines instead of 'p','y' single characters
+text = text.splitlines()
+
+# with open("data.txt","r") as f:
+#     #reading all the lines
+#     data = f.readlines()
+
+question = input("Enter your Question: ").lower()
+tempq = question
+#splitting the question into a list of words
+question = question.split()
     
     
-    for word in ignore_words:
-            #if any word we want to ignore in the question
-            if word in question:
-                #remove that word
-                question.remove(word)
+for word in ignore_words:
+    #if any word we want to ignore in the question
+    if word in question:
+        #remove that word
+        question.remove(word)
 
-    #reading each line            
-    for line in data:
-        match = 0
-        lower_line = line.lower()
-        for keyword in question:
-            #if the whole keyword is in the line
-            if keyword in lower_line.split():
-                match += 1
+#reading each line            
+for line in text: 
+    match = 0
+    lower_line = line.lower()
+    for keyword in question:
+        #if the whole keyword is in the line
+        if keyword in lower_line.split():
+            match += 1
 
-        if(match > highest):
-            highest = match
-            context = line
+    if(match > highest):
+        highest = match
+        context = line
 
 if highest == 0: 
     print("I couldn't find that info") 
@@ -62,7 +74,7 @@ Context:
 {context}
 
 Question:
-{question}
+{tempq}
 
 Answer clearly:
 """
