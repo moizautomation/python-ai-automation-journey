5ec0c5c6ff4e093500d9fc9901b9ea6d48cfbf27
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-05-05 22:47:42 +0500

    Added sidebars

--- _Projects/web_analyzer_ui.py
+++ _Projects/web_analyzer_ui.py
@@ -57,90 +57,211 @@ headers = {
     "User-Agent": "Mozilla/5.0"
 }
 
+page = st.sidebar.selectbox("Navigate", ["Home", "Tool","Instructions"])
+if page == "Home":
+    st.title("Home Page")
+    st.text("This AI tool is used to scrape data from any website\n and give you its Summary, Key Points, or the Sentiment of the Website.")
+    st.write("This is the homepage content")
+
+elif page == "Tool":
+    st.title("AI Tool")
+    st.header("Web AI Analyzer")
+    st.divider()
+
+    st.subheader("Input")
+    st.divider()
+
+    url = st.text_area("Enter the Target URL: ")
+    uploaded_file = st.file_uploader("Upload your File: ",type=["txt"])
+
+    mode = st.selectbox("Choose mode",["Summary","Key Points","Sentiment"])
+    button = st.button("Enter")
+
+    st.subheader("Output")
+    st.divider()
+
+
+    if button:
+        #Input Validation
+        if(len(url) != 0 and uploaded_file is not None):
+            st.error("Error! Cannot use both URL and File Upload at the same time")
+            st.stop()
+
+        if(len(url) == 0 and uploaded_file is None):
+            st.error("Error! Both cannot be empty")
+            st.stop()
+
+        cleaned = ""
+        key = ""
+
+        with st.spinner("Processing...."):
+            if(len(url) > 0):
+                url = url.strip()
+                key = url + mode
+                if not url.startswith("http"):
+                    st.error("Invalid URL")
+                    st.stop()
+                try:
+                    r = requests.get(url, headers=headers)
+                    soup = BeautifulSoup(r.text, "html.parser")
+
+                    data = soup.find_all(["p", "h1", "h2", "h3", "h4", "h5", "h6"])
+
+                    for el in data:
+                        cleaned += el.text.strip() + "\n"
+                except:
+                    st.error("Website cannot be reached")
+                    st.stop()
+            elif uploaded_file is not None:
+                    cleaned = uploaded_file.read().decode("utf-8").strip()
+
+                    key = cleaned[:100] + mode
+
+            if "cache" not in st.session_state:
+                st.session_state.cache = {}
+            
+            if key in st.session_state.cache:
+                response_text = st.session_state.cache[key]
+            else:
+                prompt = f"""
+        Text:
+        {cleaned}
+
+        Mode:
+        {mode}
+
+        IMPORTANT:
+        Return ONLY valid JSON.
+        """
+
+                response = model.generate_content(
+                    prompt,
+                    generation_config={"max_output_tokens": 200}
+                )
+
+                response_text = response.text
+                st.session_state.cache[key] = response_text
+                try:
+                    #used to convert string into json by reading through it
+                    # json_data = json.loads(response_text)
+
+                    #Output
+                    st.subheader("AI Result")
+                    st.write(response_text)
+                    st.download_button(
+                        label = "Download Results",
+                        data = response_text,
+                        file_name = "ai_result.json",
+                        mime="application/json"
+                    )
+                except:
+                    st.subheader("AI Result")
+                    st.error("Error! Ai return invalid result")
+
+elif page == "Instructions":
+    st.title("Instructions")
+    st.write("How to use the Tool")
+    st.write("1. Input you URL or upload File")
+    st.write("2. Choose one of the modes")
+    st.write("3. Press Enter")
+    st.write("4. Result will be shown below")
+    st.write("5. You can download it using the download button")
+
+   
+
+
+
 #Phase 1 - Input Validation and Mode setting
-st.header("Web AI Analyzer")
-st.divider()
 
-st.subheader("Input")
-st.divider()
+    # st.header("Web AI Analyzer")
+    # st.divider()
 
-url = st.text_area("Enter the Target URL: ")
-uploaded_file = st.file_uploader("Upload your File: ",type=["txt"])
+    # st.subheader("Input")
+    # st.divider()
 
-mode = st.selectbox("Choose mode",["Summary","Key Points","Sentiment"])
-button = st.button("Enter")
+    # url = st.text_area("Enter the Target URL: ")
+    # uploaded_file = st.file_uploader("Upload your File: ",type=["txt"])
 
-st.subheader("Output")
-st.divider()
+    # mode = st.selectbox("Choose mode",["Summary","Key Points","Sentiment"])
+    # button = st.button("Enter")
+
+    # st.subheader("Output")
+    # st.divider()
 
 
-if button:
-    #Input Validation
-    if(len(url) != 0 and uploaded_file is not None):
-        st.error("Error! Cannot use both URL and File Upload at the same time")
-        st.stop()
+    # if button:
+    #     #Input Validation
+    #     if(len(url) != 0 and uploaded_file is not None):
+    #         st.error("Error! Cannot use both URL and File Upload at the same time")
+    #         st.stop()
 
-    if(len(url) == 0 and uploaded_file is None):
-        st.error("Error! Both cannot be empty")
-        st.stop()
+    #     if(len(url) == 0 and uploaded_file is None):
+    #         st.error("Error! Both cannot be empty")
+    #         st.stop()
 
-    cleaned = ""
-    key = ""
+    #     cleaned = ""
+    #     key = ""
 
-    with st.spinner("Processing...."):
-        if(len(url) > 0):
-            url = url.strip()
-            key = url + mode
-            if not url.startswith("http"):
-                st.error("Invalid URL")
-                st.stop()
-            try:
-                r = requests.get(url, headers=headers)
-                soup = BeautifulSoup(r.text, "html.parser")
+    #     with st.spinner("Processing...."):
+    #         if(len(url) > 0):
+    #             url = url.strip()
+    #             key = url + mode
+    #             if not url.startswith("http"):
+    #                 st.error("Invalid URL")
+    #                 st.stop()
+    #             try:
+    #                 r = requests.get(url, headers=headers)
+    #                 soup = BeautifulSoup(r.text, "html.parser")
 
-                data = soup.find_all(["p", "h1", "h2", "h3", "h4", "h5", "h6"])
+    #                 data = soup.find_all(["p", "h1", "h2", "h3", "h4", "h5", "h6"])
 
-                for el in data:
-                    cleaned += el.text.strip() + "\n"
-            except:
-                st.error("Website cannot be reached")
-                st.stop()
-        elif uploaded_file is not None:
-                cleaned = uploaded_file.read().decode("utf-8").strip()
+    #                 for el in data:
+    #                     cleaned += el.text.strip() + "\n"
+    #             except:
+    #                 st.error("Website cannot be reached")
+    #                 st.stop()
+    #         elif uploaded_file is not None:
+    #                 cleaned = uploaded_file.read().decode("utf-8").strip()
 
-                key = cleaned[:100] + mode
+    #                 key = cleaned[:100] + mode
 
-        if "cache" not in st.session_state:
-            st.session_state.cache = {}
-        
-        if key in st.session_state.cache:
-            response_text = st.session_state.cache[key]
-        else:
-            prompt = f"""
-    Text:
-    {cleaned}
+    #         if "cache" not in st.session_state:
+    #             st.session_state.cache = {}
+            
+    #         if key in st.session_state.cache:
+    #             response_text = st.session_state.cache[key]
+    #         else:
+    #             prompt = f"""
+    #     Text:
+    #     {cleaned}
 
-    Mode:
-    {mode}
+    #     Mode:
+    #     {mode}
 
-    IMPORTANT:
-    Return ONLY valid JSON.
-    """
+    #     IMPORTANT:
+    #     Return ONLY valid JSON.
+    #     """
 
-            response = model.generate_content(
-                prompt,
-                generation_config={"max_output_tokens": 200}
-            )
+    #             response = model.generate_content(
+    #                 prompt,
+    #                 generation_config={"max_output_tokens": 200}
+    #             )
 
-            response_text = response.text
-            st.session_state.cache[key] = response_text
+    #             response_text = response.text
+    #             st.session_state.cache[key] = response_text
+    #             try:
+    #                 #used to convert string into json by reading through it
+    #                 json_data = json.loads(response_text)
 
-        #Output
-        st.subheader("AI Result")
-        st.write(response_text)
-        st.download_button(
-            label = "Download Results",
-            data = response_text,
-            file_name = "ai_result.json",
-            mime="application/json"
-        )
+    #                 #Output
+    #                 st.subheader("AI Result")
+    #                 st.write(json_data)
+    #                 st.download_button(
+    #                     label = "Download Results",
+    #                     data = json_data,
+    #                     file_name = "ai_result.json",
+    #                     mime="application/json"
+    #                 )
+    #             except:
+    #                 st.subheader("AI Result")
+    #                 st.error("Error! Ai return invalid result")
