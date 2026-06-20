4343b90f164206a967bbaf00526e861d6a4815be
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-06-20 18:13:53 +0500

    LangChain: chains, prompt templates and memory fundamentals

--- Lang Chain/fundamentals.py
+++ Lang Chain/fundamentals.py
@@ -1,3 +1,131 @@
 # Fundamentals and Basic Syntax of Langchain
+import os
+from dotenv import load_dotenv
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.chat_history import InMemoryChatMessageHistory
 
-#ChatGoogleGenerativeAI()
+load_dotenv()
+
+#choosing the ai model
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash"
+)
+
+
+#used to start the workflow of ai agent response
+# response = model.invoke(
+#     "Explain what an API is"
+# )
+
+
+# print(response.content)
+
+
+# prompt = ChatPromptTemplate.from_template(
+#     """
+# Explain this programming language:
+
+# {language}
+# """
+# )
+
+
+# the | operator is used to connect things
+# here it is used to send the promp to model
+# chain = prompt | model
+
+
+# response = chain.invoke(
+#     {
+#         "language": "Python"
+#     }
+# )
+
+# print(response.content)
+
+# TASK 1 (CREATING A SIMPLE AI RESPONSE FROM INPUT)
+# prompt = ChatPromptTemplate.from_template("""
+#     Explain the following programming language
+#     {language}
+# """
+# )
+# chain = prompt | model
+
+# response = chain.invoke(
+#     {
+#         "language" : "Python"
+#     }
+# )
+
+# print(response.content)
+
+# TASK 2 (MULTIPLE AI WORKING ON A SINGLE TASK)
+
+#make the explanation prompt
+# explain_prompt = ChatPromptTemplate.from_template("""
+#     Explain the following topic in 5 lines
+
+#     {topic}
+# """
+# )
+
+#make the bullet points prompt
+# bullet_prompt = ChatPromptTemplate.from_template("""
+#     Make the following explanation into bullet points
+
+#     {explanation}
+# """
+# )
+
+#will send the input to the model for explanation
+# explain_chain = explain_prompt | model
+
+#will run the model
+# response = explain_chain.invoke(
+#     {
+#         "topic" : "Python Language"
+#     }
+# )
+
+# will send the input from the bullet prompt to the model which is the
+# output from the previous prompt
+# bullet_chain = bullet_prompt | model
+
+# bullet_response = bullet_chain.invoke(
+#     {
+#         "explanation" : response.content
+#     }
+# )
+# print(bullet_response.content)
+
+# TASK 3 (Memory)
+
+prompt = ChatPromptTemplate.from_template(""""
+    Answer the following question:
+    {Question}
+"""
+)
+
+memory = InMemoryChatMessageHistory()
+
+memory.add_user_message("My name is Abdul Moiz")
+memory.add_ai_message("Nice to meet you Abdul Moiz")
+
+memory.add_user_message("I like Python")
+memory.add_ai_message("Great!")
+
+chain = memory | prompt
+
+chain = chain | model
+
+response = chain.invoke(
+    {
+        "Question" : "What is my name?"
+    }
+)
+
+memory.add_user_message("What is my name")
+memory.add_ai_message(response.content)
+
+print(response.content)
