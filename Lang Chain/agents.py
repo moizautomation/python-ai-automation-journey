c372e5ce8ed473b3a5bb075fb5af0d19856710fd
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-06-23 13:42:50 +0500

    Agent can now handle multiple calls in a single prompt

--- Lang Chain/agents.py
+++ Lang Chain/agents.py
@@ -26,39 +26,91 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 # print(response.tool_calls)
 
 # TASK 1 (Tool Calling Agent)
-@tool
-def calculator(a: int,b: int) -> int:
-    """Multiply two numbers"""
-    return (a * b) 
+# @tool
+# def calculator(a: int,b: int) -> int:
+#     """Multiply two numbers"""
+#     return (a * b) 
 
-@tool
-def formatted(name : str) -> str:
-    """Create a welcome message"""
-    return (f"Welcome Back, {name}")
+# @tool
+# def formatted(name : str) -> str:
+#     """Create a welcome message"""
+#     return (f"Welcome Back, {name}")
 
-query = "What is 5 * 3"
+# query = "What is 5 * 3"
 
-model_with_tools = model.bind_tools([calculator, formatted])
+# model_with_tools = model.bind_tools([calculator, formatted])
 
-response = model_with_tools.invoke(query)
+# response = model_with_tools.invoke(query)
 
-tool_calls = response.tool_calls[0]
+# tool_calls = response.tool_calls[0]
 
-result = calculator.invoke(tool_calls["args"])
+# result = calculator.invoke(tool_calls["args"])
 
-prompt = ChatPromptTemplate.from_template("""
-        The calculator returned: {result}
-        Now give the final answer to the user question: {query}
-""")
+# prompt = ChatPromptTemplate.from_template("""
+#         The calculator returned: {result}
+#         Now give the final answer to the user question: {query}
+# """)
 
-chain = prompt | model
+# chain = prompt | model
 
-final_response = chain.invoke(
-    {
-        "result" : result,
-        "query" : query
-    }
-)
+# final_response = chain.invoke(
+#     {
+#         "result" : result,
+#         "query" : query
+#     }
+# )
 
-print("Final Answer:", final_response.content)
+# print("Final Answer:", final_response.content)
 
+# TASK 2 (Multiple Calls)
+# @tool
+# def calculator(a: int,b: int) -> int:
+#     """Multiply two numbers"""
+#     return (a * b) 
+
+# @tool
+# @tool
+# def calculator(a: int,b: int) -> int:
+#     """Multiply two numbers"""
+#     return (a * b)
+
+# @tool
+# def formatted(name : str) -> str:
+#     """Create a welcome message"""
+#     return (f"Welcome Back, {name}")
+
+# query = "What is 5 * 3"
+
+# model_with_tools = model.bind_tools([calculator, formatted])
+
+# response = model_with_tools.invoke(query)
+
+# tools_map = {
+#     "calculator": calculator,
+#     "formatted": formatted
+# }
+# results = []
+
+# for tool in response.tool_calls:
+#     tools_name = tool["name"]
+#     tools_arg = tool["args"]
+#     result = tools_map[tools_name].invoke(tools_arg)
+#     results.append(f"{tools_name} returned: {result}")
+
+# results_text = "\n".join(results)
+
+# prompt = ChatPromptTemplate.from_template("""
+#         The calculator returned: {result}
+#         Now give the final answer to the user question: {query}
+# """)
+
+# chain = prompt | model
+
+# final_response = chain.invoke(
+#     {
+#         "result" : results_text,
+#         "query" : query
+#     }
+# )
+
+# print("Final Answer:", final_response.content)
