40ca4806f10031161f676960d57a5fa4985d8232
Abdul Moiz <abdulmoiz00201@gmail.com>
2026-06-23 18:38:02 +0500

    Made an agent that can search info on browser and also scrape data from websites

--- Lang Chain/agents.py
+++ Lang Chain/agents.py
@@ -2,6 +2,8 @@ from langchain_core.tools import tool
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_community.tools import DuckDuckGoSearchRun
+import requests
+from bs4 import BeautifulSoup
 import os
 from dotenv import load_dotenv
 
@@ -117,7 +119,7 @@ model = ChatGoogleGenerativeAI(model="gemini-2.5-flash")
 # print("Final Answer:", final_response.content)
 
 
-# TASK 3 (BUILD AGENT THAT Can Search WEB)
+# TASK 3 (BUILD AGENT THAT Can Search WEB and scrape websites)
 
 
 search = DuckDuckGoSearchRun()
@@ -130,14 +132,40 @@ def web_search(query: str) -> str:
     """Perform a web search for the given query"""
     return search.run(query)
 
-query = "What is the temprature in Thailand"
+@tool
+def web_scraper(url: str) -> str:
+    """Scrape the data of the website"""
+    cleaned = ""
+    if (url.startswith("http://") or url.startswith("https://")):
+        try:
+            r = requests.get(url)
+            if r.status_code != 200:
+                    return (f"Website {url} cannot be reached Error: {r.status_code}")
 
-model_with_tools = model.bind_tools([web_search])
+            
+            soup = BeautifulSoup(r.text,"html.parser")
+
+            data = soup.find_all(["h1","h2","h3","h4","h5","h6","p"])
+                    
+            for el in data:
+                cleaned += el.text.strip() + "\n"
+
+        except Exception as e:   
+            return str(e)
+    else:
+         return ("Invalid URL Provided")
+    return cleaned
+
+
+query = input("Enter your Prompt here: ")
+
+model_with_tools = model.bind_tools([web_search,web_scraper])
 
 response = model_with_tools.invoke(query)
 
 tools_map = {
-    "web_search" : web_search
+    "web_search" : web_search,
+    "web_scraper" : web_scraper
 }
 results = []
 for tools_call in response.tool_calls:
